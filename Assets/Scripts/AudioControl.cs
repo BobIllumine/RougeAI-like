e6d7694cc590d7954cad1794b4c93f6818338525
Tany Ivshina <strangetany@gmail.com>
2023-12-05 23:41:19 +0300

    Add controls display

---
+++ Assets/Scripts/AudioControl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioControl : MonoBehaviour
+{
+    private Toggle audioToggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioToggle = GetComponent<Toggle>();
+        if (AudioListener.volume == 0)
+        {
+            audioToggle.isOn = false;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ToggleAudio0nValueChange(bool audioIn)
+    {
+        if (audioIn)
+            AudioListener.volume = 1;
+        else
+            AudioListener.volume = 0;
+    }
+}
